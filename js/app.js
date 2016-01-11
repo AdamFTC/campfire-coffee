79ead41faff3870d1995389cf8c3bcb23193e666
Adam Leonard <Icepick@Adams-MacBook-Pro.local>
2016-01-11 14:19:27 -0800

    basic arrays

--- js/app.js
+++ js/app.js
@@ -1,9 +1,37 @@
 // JS goes here
 // OK I'LL try
-var ork = {
-  material: 'pewter',
+var location = {
+  site: [pike-place-market, capitol-hill, seattle-public-library, south-lake-union, sea-tac-airport, website-sales],
+  locate function()
+
+}
+
+var custperhour = {
+  custNumber: [3, 6, 14, 32, 35, 48, 49 55, 68, 75, 88, 124],
+  says function()
+}
+
+var cupspercust = {
+  cuprate: [1.1, 1.2, 1.3, 2.6, 3.2],
+  locate function()
+
+}
+
+var poundspercust = {
+  cuprate: [0.2, 0.4, 2.7, 3.7, 6.7],
+  locate function()
+
+}
+
+var hoursofoperation = {
+  hours: ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm'],
+  time function()
+
+}
+
+var customersperday = {
   color: 'green',
-  favoriteFoods: [ , , ],
+  sizes: [4, 6, 10],
   says function()
 
 }
